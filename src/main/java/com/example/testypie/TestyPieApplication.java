cbd60c98c374dd4195baf7b50a56ccc1b8b607de
tpgns4283 <ksh4283o@naver.com>
2024-01-05 15:56:20 +0900

    first commit

---
+++ src/main/java/com/example/testypie/TestyPieApplication.java
@@ -0,0 +1,13 @@
+package com.example.testypie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestyPieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestyPieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testypie/TestyPieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testypie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestyPieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
