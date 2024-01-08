263a2ef12c99b483e64acb7c4ae581e12634f45c
Yoosup Shin <brian96shin@gmail.com>
2024-01-08 16:07:06 +0900

    feat: 프로필 조회 기능 추가(이미지 조회 미구현)

--- src/main/java/com/example/testypie/user/controller/UserController.java
+++ src/main/java/com/example/testypie/user/controller/UserController.java
@@ -2,8 +2,11 @@ package com.example.testypie.user.controller;
 
 import com.example.testypie.jwt.JwtUtil;
 import com.example.testypie.user.dto.LoginRequestDTO;
-import com.example.testypie.user.dto.SignUpRequestDTO;
 import com.example.testypie.user.dto.MessageDTO;
+import com.example.testypie.user.dto.ProfileResponseDTO;
+import com.example.testypie.user.dto.SignUpRequestDTO;
+import com.example.testypie.user.entity.User;
+import com.example.testypie.user.service.UserInfoService;
 import com.example.testypie.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -11,10 +14,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -23,7 +24,7 @@ public class UserController {
 
     private final UserService userService;
 
-//    private final UserInfoService userInfoService;
+    private final UserInfoService userInfoService;
 
     private final JwtUtil jwtUtil;
 
@@ -39,10 +40,16 @@ public class UserController {
 
     //로그인
     @PostMapping("/login")
-    public ResponseEntity<MessageDTO> login(@RequestBody LoginRequestDTO req,
-                                                   HttpServletResponse res) {
+    public ResponseEntity<MessageDTO> login(@RequestBody LoginRequestDTO req, HttpServletResponse res) {
         userService.login(req);
         res.setHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(req.account()));
         return ResponseEntity.ok().body(new MessageDTO("로그인 성공", HttpStatus.OK.value()));
+    }
+
+    //프로필 조회
+    @GetMapping("/{account}")
+    public ResponseEntity<ProfileResponseDTO> getProfile(@PathVariable String account) {
+        User user = userInfoService.getUserByAccount(account);
+        return ResponseEntity.ok().body(ProfileResponseDTO.of(user));
     }
 }
