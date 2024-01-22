2c8d07674b7e0a2b3251a7a3c8a3810eb429554d
tpgns4283 <ksh4283o@naver.com>
2024-01-22 12:56:27 +0900

    product추가 시 userdetails를 제외하고 전부 넘어옴

--- src/main/java/com/example/testypie/View/controller/ViewController.java
+++ src/main/java/com/example/testypie/View/controller/ViewController.java
@@ -1,32 +1,42 @@
 package com.example.testypie.View.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping
 public class ViewController {
 
-    @GetMapping
+    @GetMapping("/home")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @GetMapping("/home/login")
+    public String loginSuccess() {
+        return "home";
     }
 
-    @GetMapping("/signup")
+    @GetMapping("/home/signup")
     public String signup() {
         return "signup";
     }
 
-    @GetMapping("/kakao-login/callback")
+    @GetMapping("/home/kakao-login/callback")
     public @ResponseBody String kakaoCallback(String code) {//Data를 리턴해주는 컨트롤러 함수
 
         return "카카오 인증 완료(코드값):" + code;
     }
+
+    @GetMapping("/api/category/{parentName}/{childId}/products")
+    public String addProduct(@PathVariable String parentName, @PathVariable Long childId, Model model) {
+        model.addAttribute("parentName", parentName);
+        model.addAttribute("childId", childId);
+        return "addProductForm"; // 이동할 뷰의 이름을 반환
+    }
 }
+
