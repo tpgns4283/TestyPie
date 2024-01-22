c7dff4fe7574b2a8ffcb9b8d34338864187fb1ec
tpgns4283 <ksh4283o@naver.com>
2024-01-22 22:22:40 +0900

    user login시 localstorage에 jwt토큰 할당

--- src/main/java/com/example/testypie/domain/user/controller/UserController.java
+++ src/main/java/com/example/testypie/domain/user/controller/UserController.java
@@ -21,7 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
+@RequestMapping
 public class UserController {
 
     private final UserService userService;
@@ -31,7 +31,7 @@ public class UserController {
     private final JwtUtil jwtUtil;
 
     //회원가입
-    @PostMapping("/signup")
+    @PostMapping("/api/users/signup")
     public ResponseEntity<MessageDTO> signup(@Valid @RequestBody SignUpRequestDTO req) {
 
         userService.signup(req);
@@ -41,7 +41,7 @@ public class UserController {
     }
 
     //로그인
-    @PostMapping("/login")
+    @PostMapping("/api/users/login")
     public ResponseEntity<MessageDTO> login(@RequestBody LoginRequestDTO req,
             HttpServletResponse res) {
         userService.login(req);
