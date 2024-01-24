bb6fc959296ed7121a09ee63ca63346dfeec8a18
BMDK9 <qnrakdlzm77@naver.com>
2024-01-24 17:27:00 +0900

    #58 feat: Product 좋아요 기능 구현

---
+++ src/main/java/com/example/testypie/domain/productLike/dto/ProductLikeResponseDto.java
@@ -0,0 +1,11 @@
+package com.example.testypie.domain.productLike.dto;
+
+public record ProductLikeResponseDto(
+        Boolean isProductLiked
+) {
+
+    public static ProductLikeResponseDto of(Boolean isProductLiked) {
+
+        return new ProductLikeResponseDto(isProductLiked);
+    }
+}
