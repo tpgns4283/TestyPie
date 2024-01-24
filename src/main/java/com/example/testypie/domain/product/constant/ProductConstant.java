fc139bf39a13a75b6dedd6d9af3d3dc1b4f867e7
BMDK9 <qnrakdlzm77@naver.com>
2024-01-25 00:08:13 +0900

    #58 chore: 오타, 서식 및 패키지 수정

--- src/main/java/com/example/testypie/domain/product/constant/ProductConstant.java
+++ src/main/java/com/example/testypie/domain/product/constant/ProductConstant.java
@@ -2,6 +2,5 @@ package com.example.testypie.domain.product.constant;
 
 public class ProductConstant {
 
-    public static final Long DEFAULT_LIKE_CNT = 0L;
-    public static final Boolean DEFAULT_PRODUCT_LIKE = false;
+    public static final Long DEFAULT_PRODUCT_LIKE_CNT = 0L;
 }
