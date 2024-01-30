141e6c2007673aa7f35b258dacb30c42fac45885
BMDK9 <qnrakdlzm77@naver.com>
2024-01-30 16:07:24 +0900

    chore: S3Uploader 삭제 관련

--- src/main/java/com/example/testypie/domain/user/service/UserInfoService.java
+++ src/main/java/com/example/testypie/domain/user/service/UserInfoService.java
@@ -8,7 +8,6 @@ import com.example.testypie.domain.reward.entity.Reward;
 import com.example.testypie.domain.user.dto.*;
 import com.example.testypie.domain.user.entity.User;
 import com.example.testypie.domain.user.repository.UserRepository;
-import com.example.testypie.domain.util.S3Uploader;
 import com.example.testypie.domain.util.S3Util;
 import com.example.testypie.domain.util.S3Util.FilePath;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +16,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Random;
