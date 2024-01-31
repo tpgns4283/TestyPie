9955f25e07d8343dd1d45f867bffb27540c57cd7
Yoosup Shin <brian96shin@gmail.com>
2024-01-31 11:31:39 +0900

    refactor: url 주소 입력시 바로 home으로 넘어갈수있게설정

--- src/main/java/com/example/testypie/View/controller/ViewController.java
+++ src/main/java/com/example/testypie/View/controller/ViewController.java
@@ -1,14 +1,11 @@
 package com.example.testypie.View.controller;
 
 import com.example.testypie.domain.user.dto.ProfileResponseDTO;
-import com.example.testypie.domain.user.entity.RefreshToken;
 import com.example.testypie.domain.user.entity.User;
 import com.example.testypie.domain.user.kakao.service.KakaoService;
-import com.example.testypie.domain.user.service.RefreshTokenService;
 import com.example.testypie.domain.user.service.UserInfoService;
 import com.example.testypie.global.jwt.JwtUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.nimbusds.openid.connect.sdk.claims.UserInfo;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,8 +14,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -34,7 +33,7 @@ public class ViewController {
     private final UserInfoService userInfoService;
     private final JwtUtil jwtUtil;
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home(Model model, HttpServletRequest request) {
         User user = (User) request.getSession().getAttribute("LOGGED_IN_USER");
         // 사용자 정보가 있다면, 모델에 추가
