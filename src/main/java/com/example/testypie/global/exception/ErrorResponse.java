a3b9a70f5ba9e409df9366219c1afe1f8c3a43b4
Yoosup Shin <brian96shin@gmail.com>
2024-02-02 10:16:25 +0900

    feat: ErrorResponse 정의 완료

--- src/main/java/com/example/testypie/global/exception/ErrorResponse.java
+++ src/main/java/com/example/testypie/global/exception/ErrorResponse.java
@@ -8,6 +8,7 @@ import org.springframework.validation.FieldError;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
@@ -87,4 +88,36 @@ public class ErrorResponse {
         return new ErrorResponse(code, reason);
     }
 
+    /**
+     * 에러를 e.getBindingResult() 형태를 전달 받는 경우 해당 내용을 상세 내용으로 변경하는 기능
+     */
+    @Getter
+    public static class FieldError {
+        private final String field;
+        private final String value;
+        private final String reason;
+
+        public static List<FieldError> of(final String field, final String value, final String reason) {
+            List<FieldError> fieldErrors = new ArrayList<>();
+            fieldErrors.add(new FieldError(field, value, reason));
+            return fieldErrors;
+        }
+
+        private static List<FieldError> of(final BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()))
+                    .collect(Collectors.toList());
+        }
+
+        @Builder
+        public FieldError(String field, String value, String reason) {
+            this.field = field;
+            this.value = value;
+            this.reason = reason;
+        }
+    }
 }
