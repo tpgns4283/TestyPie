4bf7e509625971e87779513ad08f5ec9d384e176
BMDK9 <qnrakdlzm77@naver.com>
2024-02-05 20:51:58 +0900

    #131 refactor: RewardCreateRequestDTO 아이템 사이즈 최소 0

--- src/main/java/com/example/testypie/domain/reward/dto/RewardCreateRequestDTO.java
+++ src/main/java/com/example/testypie/domain/reward/dto/RewardCreateRequestDTO.java
@@ -1,6 +1,6 @@
 package com.example.testypie.domain.reward.dto;
 
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.NonNull;
 
-public record RewardCreateRequestDTO(@NonNull String rewardItem, @NonNull @Min(0) Long itemSize) {}
+public record RewardCreateRequestDTO(@NonNull String rewardItem, @NonNull @Size Long itemSize) {}
