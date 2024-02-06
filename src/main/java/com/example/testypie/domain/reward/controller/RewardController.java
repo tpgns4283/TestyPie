c481dec17060a3dec89381b53c056db17afb2873
BMDK9 <qnrakdlzm77@naver.com>
2024-02-06 17:12:49 +0900

    #131 chore: reward 첨삭

--- src/main/java/com/example/testypie/domain/reward/controller/RewardController.java
+++ src/main/java/com/example/testypie/domain/reward/controller/RewardController.java
@@ -1,17 +1,14 @@
 package com.example.testypie.domain.reward.controller;
 
 import com.example.testypie.domain.reward.dto.response.DeleteRewardResponseDTO;
-import com.example.testypie.domain.reward.dto.response.ReadRewardResponseDTO;
 import com.example.testypie.domain.reward.service.RewardService;
 import com.example.testypie.domain.user.entity.User;
 import com.example.testypie.domain.user.service.UserInfoService;
 import com.example.testypie.global.security.UserDetailsImpl;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,22 +21,14 @@ public class RewardController {
   private final RewardService rewardService;
   private final UserInfoService userInfoService;
 
-  @GetMapping("/reward")
-  public ResponseEntity<List<ReadRewardResponseDTO>> getRewardList(
-      @AuthenticationPrincipal UserDetailsImpl userDetails) {
-
-    List<ReadRewardResponseDTO> resList = rewardService.getRewardList(userDetails.getUser());
-    return ResponseEntity.ok(resList);
-  }
-
-  @GetMapping("/reward/{account}")
-  public ResponseEntity<List<ReadRewardResponseDTO>> getReward(
-      @AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable String account) {
-
-    userInfoService.checkSameUser(account, userDetails.getUsername());
-    List<ReadRewardResponseDTO> resList = rewardService.getReward(account);
-    return ResponseEntity.ok(resList);
-  }
+  //  사용 예정 (랜덤 로직)
+  //  @GetMapping("/reward")
+  //  public ResponseEntity<List<ReadRewardResponseDTO>> getRewardList(
+  //      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+  //
+  //    List<ReadRewardResponseDTO> resList = rewardService.getRewardList(userDetails.getUser());
+  //    return ResponseEntity.ok(resList);
+  //  }
 
   @DeleteMapping("/reward/{account}/{reward_id}")
   public ResponseEntity<DeleteRewardResponseDTO> deleteReward(
